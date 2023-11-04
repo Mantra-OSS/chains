14c8bddeba8d3cb0d5ca266de57cdbd3ed10f029
JC <jc@pnkr.io>
2023-11-04 23:50:21 +0300

    asd

--- package.d.ts
+++ package.d.ts
@@ -1,10 +1,6 @@
-/** @type {URL} */
-export const dataPath = new URL("./_data/", import.meta.url)
-/** @type {URL} */
-export const chainsDataPath = new URL("./chains/", dataPath)
-/** @type {URL} */
-export const iconsDataPath = new URL("./icons/", dataPath)
-/** @type {URL} */
-export const iconsDownloadDataPath = new URL("./iconsDownload/", dataPath)
-/** @type {typeof import("./tools/schema/chainSchema.json")} */
-export { default as chainSchema } from "./tools/schema/chainSchema.json" assert { type: "json" }
+export declare const dataPath: URL;
+export declare const chainsDataPath: URL;
+export declare const iconsDataPath: URL;
+export declare const iconsDownloadDataPath: URL;
+export { default as chainSchema } from "./tools/schema/chainSchema.json";
+export type * from "./chain.ts";
--- package.js
+++ package.js
@@ -1,10 +1,5 @@
-/** @type {URL} */
-export const dataPath = new URL("./_data/", import.meta.url)
-/** @type {URL} */
-export const chainsDataPath = new URL("./chains/", dataPath)
-/** @type {URL} */
-export const iconsDataPath = new URL("./icons/", dataPath)
-/** @type {URL} */
-export const iconsDownloadDataPath = new URL("./iconsDownload/", dataPath)
-/** @type {typeof import("./tools/schema/chainSchema.json")} */
-export { default as chainSchema } from "./tools/schema/chainSchema.json" assert { type: "json" }
+export const dataPath = new URL("./_data/", import.meta.url);
+export const chainsDataPath = new URL("./chains/", dataPath);
+export const iconsDataPath = new URL("./icons/", dataPath);
+export const iconsDownloadDataPath = new URL("./iconsDownload/", dataPath);
+export { default as chainSchema } from "./tools/schema/chainSchema.json" assert { type: "json" };
